9c3b4121964163b1fbbb79a456b16b6a5e94ef0a
sergiuhruban <hrubansergiu@gmail.com>
2023-03-06 21:06:24 +0200

    functions

---
+++ functions/main.py
@@ -0,0 +1,77 @@
+# def my_first_function():
+#     print("Hello from my first function")
+#
+#     print("sum of 5 + 7", 5 + 7)
+#
+#
+#
+# print('--BEFORE FUNCTION CALL')
+# my_first_function()
+# print('--AFTER FUNCTION CALL')
+
+# def show_sum_of_numbers():
+#     while True:
+#         try:
+#             a = int(input("a = "))
+#             break
+#         except ValueError:
+#             print("Value for a is not a number. Try again")
+#
+#     while True:
+#         try:
+#             b = int(input("b = "))
+#             break
+#         except ValueError:
+#             print("Value for a is not a number. Try again")
+#
+#
+#     print(f"Sum of {a} and {b} is {a + b}")
+#
+#
+# show_sum_of_numbers()
+# show_sum_of_numbers()
+# show_sum_of_numbers()
+
+
+
+# def get_number():
+#     number = int(input("number: "))
+#     return number
+#
+# a = get_number()
+# b = get_number()
+#
+# print("Sum = ", a + b)
+
+
+
+
+
+
+
+
+
+
+# def get_number_from_user():
+#     number = in(input("number:"))
+#
+# def show_sum_of_numbers():
+#     while True:
+#         try:
+#             get_number_from_user()
+#             break
+#         except ValueError:
+#             print("Value for a is not a number. Try again")
+#
+#     while True:
+#         try:
+#             get_number_from_user()
+#             break
+#         except ValueError:
+#             print("Value for a is not a number. Try again")
+#
+#
+#     print(f"Sum of {a} and {b} is {a + b}")
+#
+#
+# show_sum_of_numbers()
