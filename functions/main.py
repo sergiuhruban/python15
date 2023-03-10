01d16141cd3631dacc2f5b80ee2ab429aacddee4
sergiuhruban <hrubansergiu@gmail.com>
2023-03-11 00:07:11 +0200

    functions

--- functions/main.py
+++ functions/main.py
@@ -45,13 +45,6 @@
 
 
 
-
-
-
-
-
-
-
 # def get_number_from_user():
 #     number = in(input("number:"))
 #
@@ -75,3 +68,70 @@
 #
 #
 # show_sum_of_numbers()
+
+# def do_math(a, b, operation="+"):
+#     if operation == "*":
+#         return a * b
+#
+#     if operation == "/":
+#         return a / b
+#
+#     if operation == "-":
+#         return a - b
+#
+#     return a + b
+#
+# print(do_math(3, 7))
+# print(do_math(4, 7, operation="*"))
+
+
+# def f(a, b ,c, *args): #variable length arguments
+#     print(a, b, c, args)
+#
+# # f()
+# # f(10, 5)
+# f(10, 5, 6, 7, 9)
+# f(4, 5, 6, 7, 78, 8)
+
+
+# def f(*args, **kwargs): #keyword variable length arguments
+#     print(args, kwargs)
+#
+# f()
+# f(1, 2, 3)
+# f(1, 2, 3, 4)
+# f(1, 2, 3, 4, 5, p1=2, p2="abc")
+# f(1, 2, 3, 43, 4, p1=2, p3="123", p4=[1,2])
+
+# def f(a, b):
+#     return a + b
+#
+# a = f
+#
+# print(a(2, 7))
+
+# my_variable = 10
+#
+# def f1():
+#     print("my variable inside f1"my_variable)
+#
+#
+# f1()
+
+# def f():
+#     print("inside")
+#     f()
+# f()
+
+# def f(n):
+#     print(n)
+#
+#
+#
+#     if n == 0:
+#         return
+#
+#     f(n - 1)
+#
+# f(5)
+
