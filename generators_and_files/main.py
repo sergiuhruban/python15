79fa92d7a4baf4746dfbfe43488526cd2c1116d9
sergiuhruban <hrubansergiu@gmail.com>
2023-04-02 18:02:05 +0300

    generators_and_files

---
+++ generators_and_files/main.py
@@ -0,0 +1,42 @@
+# def get_numbers():
+#    for element in range(10):
+#        yield element
+#
+#
+# # my_sequence = get_numbers()
+#
+# # for element in my_sequence:
+#
+# print(next(my_sequence))
+
+# def get_numbers():
+
+# import sys
+#
+#
+# my_list = [element for element in range(10)]
+# print(type(my_list), sys.getsizeof(my_list))
+#
+# my_generator = (element for element in range(10))
+# print(type(my_generator), sys.getsizeof(my_generator))
+
+
+
+# if __name__ == '__main__':
+#     # file = open("/Users/sergiuhruban/PycharmProjects/python15/generators_and_files/data.txt", "r")
+#     file = open("data.txt", "r")
+#     line = file.readline()
+#     print(line)
+#     line = file.readline()
+#     print(line)
+#     file.close()
+#
+#
+#     line = file.readline()
+#     print(line)
+
+
+
+
+
+
